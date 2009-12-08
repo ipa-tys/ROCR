0af643cb05b6775c0e8e2470779ac7e64104d5f5
Tobias Sing <tobias.sing@mpi-sb.mpg.de>
2009-12-08 00:00:00 +0000

    version 1.0-4

--- R/zzz.R
+++ R/zzz.R
@@ -26,12 +26,26 @@ setClass("performance",
 
 setMethod("plot",signature(x="performance",y="missing"),
           function(x,y,..., avg="none", spread.estimate="none",
-  spread.scale=1, show.spread.at=c(), colorize=F,
+  spread.scale=1, show.spread.at=c(), colorize=FALSE,
   colorize.palette=rev(rainbow(256,start=0, end=4/6)),
   colorkey=colorize, colorkey.relwidth=0.25, colorkey.pos="right",
   print.cutoffs.at=c(), cutoff.label.function=function(x) { round(x,2) },
   downsampling=0, add=FALSE ) {
-              .plot.performance(x,...)
+
+              .plot.performance(x,..., 
+              					avg= avg, 
+              					spread.estimate= spread.estimate,
+								spread.scale= spread.scale, 
+								show.spread.at= show.spread.at, 
+								colorize= colorize,
+								colorize.palette= colorize.palette,
+								colorkey= colorkey, 
+								colorkey.relwidth= colorkey.relwidth, 
+								colorkey.pos= colorkey.pos,
+								print.cutoffs.at= print.cutoffs.at, 
+								cutoff.label.function= cutoff.label.function,
+								downsampling= downsampling, 
+								add= add)
           })
 
 
