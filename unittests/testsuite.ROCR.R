62dfb1951ecb22c24a287e3be995dfe4100f0ffb
Billy Aung Myint <r@use-r.com>
2015-03-22 00:00:00 +0000

    version 1.0-6

--- unittests/testsuite.ROCR.R
+++
@@ -1,9 +0,0 @@
-library(RUnit)
-library(ROCR)
-
-myTestSuite <- defineTestSuite("ROCR test suite", "unittests")
-isValidTestSuite(myTestSuite)
-testData <- runTestSuite(myTestSuite)
-
-printTextProtocol(testData, showDetails=TRUE)
-printHTMLProtocol(testData, "unittests/testresults.html")
